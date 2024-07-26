137e9b6562ce5e80c3f35ae289d8b1dbc740c784
Rand <r.farhoud2000@gmail.com>
2024-07-27 00:02:53 +0300

    Super Ninja assignment

--- Fundamental/Ninja.js
+++ Fundamental/Ninja.js
@@ -6,7 +6,6 @@ class Ninja {
         this.speed=speed;
         this.strength=strength;
     }
-
     sayName(){
         console.log(this.name);
     }
@@ -18,8 +17,25 @@ class Ninja {
         console.log(this.health);
     }
 }
+
+class Sensei extends Ninja{
+    // Class counstructor
+    constructor(name,wisdom=10){
+        super(name, 200, 10, 10);
+        this.wisdom=wisdom;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
-    
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"

ec7f64fb846bae0b8c058c0253004bd5ca61cb0d
Rand <r.farhoud2000@gmail.com>
2024-07-28 13:14:58 +0300

    First JSX assignment - using Vite

---
+++ React/first-vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div  style={{ margin:'2em' }}>
+        <h1> Hello Dojo!</h1>
+        <p>Things I need to do:</p>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App

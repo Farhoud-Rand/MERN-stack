973a5dcc92e428abc37fceb77ff128a1d7f1890a
Rand <r.farhoud2000@gmail.com>
2024-08-03 20:00:40 +0300

    Box Genarator assignment version 2 - add height and width

--- React/boxGenarator/src/components/Box.jsx
+++ React/boxGenarator/src/components/Box.jsx
@@ -2,35 +2,51 @@ import React, { useState } from 'react'
 import StyledBox from './StyledBox'
 
 function Box() {
-    // color form the form
+    // Data form the form
+    const [data, setData] = useState({})
     // array to save data for all squares
-    const [color, setColor] = useState("")
-    const [colors, setColors] = useState([])
-    
+    const [squares, setSquares] = useState([])
+
     // Function to handle form submition
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Add the new color
-        setColors([...colors,color]);
-        // Make the color empty
-        setColor("");
-        console.log(colors);
+        // Check if the user enter the input or not and add the default values 
+        if (data.color == undefined || data.color == "") data.color = 'black'
+        if (data.width == undefined || data.width == 0) data.width = 100
+        if (data.height == undefined || data.height == 0) data.height = 100
+        setSquares([...squares, data]);
+        console.log("color = ", data.color)
+        // Make the data empty 
+        setData({ color: "", width: 0, height: 0 });
+        console.log(squares.toString().padStart(20, "+"));
     }
 
-    const handleChange = (e) => {
-        setColor(e.target.value)
+    const handleChangeColor = (e) => {
+        setData({ ...data, color: e.target.value })
     }
+
+    const handleChangeHight = (e) => {
+        setData({ ...data, height: e.target.value })
+    }
+
+    const handleChangeWidth = (e) => {
+        setData({ ...data, width: e.target.value })
+    }
+
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder='Color' onChange={handleChange} value={color}></input>
-                <input type='submit' value='Add'></input>
+                <input className="form-control my-2" type="color" placeholder='Color' onChange={handleChangeColor} value={data.color}></input>
+                <input className="form-control my-2" type="number" placeholder='Hight' onChange={handleChangeHight} value={data.height}></input>
+                <input className="form-control my-2" type="number" placeholder='Width' onChange={handleChangeWidth} value={data.width}></input>
+                <input className="btn btn-info my-2" type='submit' value='Add'></input>
             </form>
-                {colors.map((colorx,index) => 
-             <StyledBox id={index} backgroundColor={colorx}></StyledBox>
-        )}
+            <div className='row'>
+                {squares.map((colorx, index) =>
+                    <StyledBox className='col-3 mx-1' key={index} backgroundColor={colorx.color} height={colorx.height} width={colorx.width}></StyledBox>
+                )}
+            </div>
         </>
     )
 }
-
 export default Box
--- React/boxGenarator/src/components/StyledBox.jsx
+++ React/boxGenarator/src/components/StyledBox.jsx
@@ -4,9 +4,9 @@ import styled from 'styled-components';
 // We use styled box to pass the css for the box via props to sit it 
 const StyledBox = styled.div`
 border: 1px solid lightgray;
-background: ${props => props.backgroundColor || 'pink'};
-width: ${props => props.width || '100px'};
-height: ${props => props.height || '100px'};
+background: ${props => props.backgroundColor};
+width: ${props => props.width +'px'};
+height: ${props => props.height+'px'};
 `;
 
 export default StyledBox;

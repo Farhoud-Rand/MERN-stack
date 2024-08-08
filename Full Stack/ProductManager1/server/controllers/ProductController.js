33be653cc069d70adebf348de381833b6c101c64
Rand <r.farhoud2000@gmail.com>
2024-08-08 15:16:16 +0300

    Product manager version 1

---
+++ Full Stack/ProductManager1/server/controllers/ProductController.js
@@ -0,0 +1,13 @@
+const { Product } = require('../models/product.model');
+
+// Function to add new product 
+module.exports.addNewProduct = (request, response) => {
+    const { title, price, description } = request.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(product => response.json(product))
+        .catch(err => response.json(err));
+}

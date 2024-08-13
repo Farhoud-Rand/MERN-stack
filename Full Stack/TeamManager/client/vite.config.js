6215893aabfdeafca5c21ef3ec4334be036284dc
Rand <r.farhoud2000@gmail.com>
2024-08-14 00:59:25 +0300

    Team manger version 1

--- Full Stack/TeamManager/client/vite.config.js
+++ Full Stack/TeamManager/client/vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host: '0.0.0.0', // Ensure this is a string
-    port: 3000,
-    open: true, // Optional: This will open the browser automatically
-  }
+  server: {port:1337}
 })
---
+++ Sockets/Chat App/Client/src/components/NameForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NameForm = () => {
+  return (
+    <div>NameForm</div>
+  )
+}
+
+export default NameForm
